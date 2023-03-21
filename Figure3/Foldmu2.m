8f4cd20dc172f02e41e238122dffd048e45d2e62
Tracy Stepien <tstepien@ufl.edu>
2023-03-21 14:57:23 -0400

    add vertical mu=0.03 line

--- Figure3/Foldmu2.m
+++ Figure3/Foldmu2.m
@@ -19,6 +19,7 @@ foldTL150 = csvread('Data/Foldmu_beta_1_5_TL.csv');
 
 %%% Plots
 
+fs = 22;
 
 figure(1)
 plot(foldR050(:,2),foldR050(:,1),'b','LineWidth',2)
@@ -27,10 +28,23 @@ hold on
 plot(foldTR050(:,2),foldTR050(:,1),'b','LineWidth',2)
 plot(foldTL050(:,2),foldTL050(:,1),'b','LineWidth',2)
 plot(linspace(0,1,100),ones(100,1)*(1/0.5), 'k-.')
+xline(0.03,':')
 hold off
 xlabel('\mu')
-ylabel('\alpha','rotation',0)
+ylab1 = ylabel('\alpha','rotation',0);
+
 axis([0 1 0 4])
+annotation('textbox',[0.2054 0.7476 0.0464 0.0609],...
+    'String',{'2'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.6455 0.2519 0.0590 0.0632],...
+    'String',{'2'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.4964 0.6547 0.0590 0.0632],...
+    'String',{'3'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.2436 0.3433 0.0591 0.0632],...
+    'String',{'3'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+
+ylab1.Position(1) = ylab1.Position(1) - 0.03;  % move the label to the left
+
 
 
 figure(2)
@@ -40,8 +54,21 @@ hold on
 plot(foldTR150(:,2),foldTR150(:,1),'b','LineWidth',2)
 plot(foldTL150(:,2),foldTL150(:,1),'b','LineWidth',2)
 plot(linspace(0,1,100),ones(100,1)*(1/1.5), 'k-.')
+xline(0.03,':')
 hold off
 xlabel('\mu')
-ylabel('\alpha','rotation',0)
+ylab2 = ylabel('\alpha','rotation',0);
 axis([0 1 0 4])
 
+annotation('textbox',[0.1197 0.4681 0.0580 0.0643],...
+    'String',{'1'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.2357 0.6442 0.0580 0.0643],...
+    'String',{'2'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.6750 0.1799 0.0580 0.0643],...
+    'String',{'2'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.6071 0.5561 0.0580 0.0643],...
+    'String',{'3'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+annotation('textbox',[0.2786 0.1871 0.0580 0.0643],...
+    'String',{'3'},'FontSize',fs,'EdgeColor','none','FontWeight','bold');
+
+ylab2.Position(1) = ylab2.Position(1) - 0.03;  % move the label to the left
